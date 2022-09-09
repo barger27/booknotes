82f2b8e89712fe7686cf4d05312f147a22985afe
Ryan Barger <ryan.barger@ingagepartners.com>
2022-09-08 22:48:45 -0400

    Initial commit

---
+++ Source/Book Notes/Book Notes/BookNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_NotesApp.swift
+//  Book Notes
+//
+//  Created by Ryan Barger on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct BookNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
